44ce220403f0d6b3759683fb0b176c187935b953
Ravi Kumar <ravi.kumar@smartdatainc.net>
2019-12-17 16:52:55 +0530

    all provider types integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sample_provider/response_display.dart';
-import 'package:sample_provider/text_display.dart';
-import 'package:sample_provider/text_edit.dart';
-import 'app_state.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: ChangeNotifierProvider<AppState>(
-          create: (_) => AppState(),
-          child: MyHomePage(),
-        ));
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final appState = Provider.of<AppState>(context);
-
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: ListView(
+    return MultiProvider(
+      //                                     <--- MultiProvider
+      providers: [
+        ChangeNotifierProvider<MyModel>(create: (context) => MyModel()),
+        ChangeNotifierProvider<AnotherModel>(
+            create: (context) => AnotherModel()),
+      ],
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('My App')),
+          body: Column(
             children: <Widget>[
-              TextDisplay(),
-              TextEditWidget(),
-              RaisedButton(
-                onPressed: () => appState.fetchData(),
-                child: Text("Fetch Data from Network"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      padding: const EdgeInsets.all(20),
+                      color: Colors.green[200],
+                      child: Consumer<MyModel>(
+                        //            <--- MyModel Consumer
+                        builder: (context, myModel, child) {
+                          return RaisedButton(
+                            child: Text('Do something'),
+                            onPressed: () {
+                              // We have access to the model.
+                              myModel.doSomething();
+                            },
+                          );
+                        },
+                      )),
+                  Container(
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.blue[200],
+                    child: Consumer<MyModel>(
+                      //              <--- MyModel Consumer
+                      builder: (context, myModel, child) {
+                        return Text(myModel.someValue);
+                      },
+                    ),
+                  ),
+                ],
               ),
-              ResponseDisplay(),
+
+              // SizedBox(height: 5),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      padding: const EdgeInsets.all(20),
+                      color: Colors.red[200],
+                      child: Consumer<AnotherModel>(
+                        //      <--- AnotherModel Consumer
+                        builder: (context, myModel, child) {
+                          return RaisedButton(
+                            child: Text('Do something'),
+                            onPressed: () {
+                              myModel.doSomething();
+                            },
+                          );
+                        },
+                      )),
+                  Container(
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.yellow[200],
+                    child: Consumer<AnotherModel>(
+                      //        <--- AnotherModel Consumer
+                      builder: (context, anotherModel, child) {
+                        return Text('${anotherModel.someValue}');
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class MyModel with ChangeNotifier {
+  //                        <--- MyModel
+  String someValue = 'Hello';
+  void doSomething() {
+    someValue = 'Goodbye';
+    print(someValue);
+    notifyListeners();
+  }
+}
+
+class AnotherModel with ChangeNotifier {
+  //                   <--- AnotherModel
+  int someValue = 0;
+  void doSomething() {
+    someValue = 5;
+    print(someValue);
+    notifyListeners();
   }
 }
--- lib/response_display.dart
+++ lib/response_display.dart
@@ -9,7 +9,6 @@ class ResponseDisplay extends StatelessWidget {
     return Container(
         padding: const EdgeInsets.all(16.0),
         child: Consumer<AppState>(
-          //<--- Consumer
           builder: (context, appState, child) {
             return Container(
                 child: appState.isFetching
@@ -35,28 +34,5 @@ class ResponseDisplay extends StatelessWidget {
                         : Text("Press Button above to fetch data"));
           },
         ));
-    // Container(
-    //   padding: const EdgeInsets.all(16.0),
-    //   child: appState.isFetching
-    //       ? CircularProgressIndicator()
-    //       : appState.getResponseJson() != null
-    //           ? ListView.builder(
-    //               primary: false,
-    //               shrinkWrap: true,
-    //               itemCount: appState.getResponseJson().length,
-    //               itemBuilder: (context, index) {
-    //                 return ListTile(
-    //                   leading: CircleAvatar(
-    //                     backgroundImage: NetworkImage(
-    //                         appState.getResponseJson()[index]['avatar']),
-    //                   ),
-    //                   title: Text(
-    //                     appState.getResponseJson()[index]["first_name"],
-    //                   ),
-    //                 );
-    //               },
-    //             )
-    //           : Text("Press Button above to fetch data"),
-    // );
   }
 }
